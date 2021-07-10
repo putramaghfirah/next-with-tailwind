56b2fb71bde8f05c79d99995ef03dadc4634ae2a
putramaghfirah <putra.maghfirah@gmail.com>
2021-07-10 07:48:21 +0700

    🐛 fix: fix bug localstorage

--- src/lib/useDarkMode.ts
+++ src/lib/useDarkMode.ts
@@ -2,7 +2,9 @@ import { useState, useEffect } from 'react'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function useDarkMode() {
-  const [theme, setTheme] = useState(null)
+  const [theme, setTheme] = useState(
+    typeof window !== 'undefined' ? localStorage.getItem('theme') : null,
+  )
 
   const colorTheme = theme === 'dark' ? 'light' : 'dark'
 
